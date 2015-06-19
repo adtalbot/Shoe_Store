aedf769f79e0d27e08294c4fc51c6570e6a3fb2c
audrie <audrie.talbot@gmail.com>
2015-06-19 10:14:24 -0700

    Passes spec for capitalize method for shoe class.

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -9,4 +9,10 @@ describe(Shoe) do
     test_shoe = Shoe.new({:name => ""})
     expect(test_shoe.save()).to(eq(false))
   end
+  describe('#capitalize') do
+    it('capitalizes the first letter of shoe name') do
+      test_shoe = Shoe.new({:name => 'nike'})
+      expect(test_shoe.name().capitalize()).to(eq('Nike'))
+    end
+  end
 end
