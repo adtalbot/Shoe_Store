7db72a1cb1d91805086aebfc8d4d1c62f7bc7f8e
audrie <audrie.talbot@gmail.com>
2015-06-19 10:06:48 -0700

    Passes spec for validation method for shoe and store classes.

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -3,3 +3,10 @@ require('spec_helper')
 describe(Shoe) do
   it{should have_and_belong_to_many(:stores)}
 end
+
+describe(Shoe) do
+  it('validates the presence of name') do
+    test_shoe = Shoe.new({:name => ""})
+    expect(test_shoe.save()).to(eq(false))
+  end
+end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,3 +3,10 @@ require('spec_helper')
 describe(Store) do
   it{should have_and_belong_to_many(:shoes)}
 end
+
+describe(Store) do
+  it('validates the presence of name') do
+    test_store = Store.new({:name => ""})
+    expect(test_store.save()).to(eq(false))
+  end
+end
